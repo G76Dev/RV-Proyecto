e9f1ba4beb2f0bbdbff4a265d97eb891296b927f
Thund3rDev <samuelriosdev@gmail.com>
2020-05-07 20:44:47 +0200

    Rip

--- Proyecto RV/Assets/Scripts/VirtualMarker.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class VirtualMarker : MonoBehaviour
-{
-    public List<int> marcadoresAlrededor = new List<int>();
-}
