dbffefc5b05747f8a84342bbb75386b0910daa2a
Thund3rDev <samuelriosdev@gmail.com>
2020-05-07 14:51:02 +0200

    Ahora deberia ir

---
+++ Proyecto RV/Assets/Scripts/VirtualMarker.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualMarker : MonoBehaviour
+{
+    public List<int> marcadoresAlrededor = new List<int>();
+}
