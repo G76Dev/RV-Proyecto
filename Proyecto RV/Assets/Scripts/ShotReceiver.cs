db0bf6efdaa040d2a94fdb64ad0c29968151308b
Fernando <fernandomartin4@gmail.com>
2020-05-22 17:30:50 +0200

    Disparo al maletín

--- Proyecto RV/Assets/Scripts/ShotReceiver.cs
+++ Proyecto RV/Assets/Scripts/ShotReceiver.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class ShotReceiver : MonoBehaviour
 {
-    
+
+    [Tooltip("Distancia a la que se puede reaccionar al disparo")]
+    [SerializeField] private float distanceToBeShot;
+
+    private bool isShot = false;
+
     public void Receive()
     {
-        Debug.Log("Me han disparado.");
+        if ((transform.position - Camera.main.transform.position).magnitude < distanceToBeShot)
+            isShot = true;
+    }
+
+    public bool IsShot()
+    {
+        return isShot;
+    }
+
+    public void AlreadyShot()
+    {
+        isShot = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distanceToBeShot);
     }
 
 }
---
+++ Proyecto RV/Assets/SuitcaseBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SuitcaseBehaviour : MonoBehaviour
+{
+
+    [Tooltip("Referencia al script 'ShotReceiver' del mismo game object")]
+    [SerializeField] private ShotReceiver sr;
+
+    [Tooltip("Referencia al Animator de Timeline del maletín")]
+    [SerializeField] private PlayableDirector pd;
+
+    private void Update()
+    {
+        if (sr.IsShot())
+        {
+            // ...
+            pd.Play();
+            sr.AlreadyShot();
+            gameObject.SetActive(false);
+        }
+    }
+
+}
